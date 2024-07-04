3b5fb8501ecc96ea7f2428bfc8013a39151027f4
ZairBulos <buloszair@gmail.com>
2024-07-04 19:03:41 -0300

    feat(client): add BookList and BookDetail

---
+++ client/src/components/BookCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="book-card">
+    <img :src="book.cover" :alt="book.title">
+    <h3>{{ book.title }}</h3>
+    <p>{{ `${book.author.last_name}` + ', ' + `${book.author.first_name}` }}</p>
+  </div>
+</template>
+
+<style>
+.book-card {
+  padding: 16px;
+  margin: 8px;
+  text-align: center;
+  width: 200px;
+
+  & img {
+    width: 100%;
+    height: 50vh;
+  }
+}
+</style>
+
+<script>
+export default {
+  props: {
+    book: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,10 @@
 import { createStore } from 'vuex';
 import bookModule from '../modules/book';
-import authorModule from '../modules/author';
 import categoryModule from '../modules/category';
 
 export default createStore({
   modules: {
     books: bookModule,
-    authors: authorModule,
     categories: categoryModule
   }
 });
